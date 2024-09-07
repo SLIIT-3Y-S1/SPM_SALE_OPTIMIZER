6a077a2cd83139d3ae21935f3004ecf863663d4b
gamithf <gamithfernando311@gmail.com>
2024-09-07 18:28:20 +0530

    Updated the inventory home

--- client/src/app/(pages)/Inventory/page.tsx
+++ client/src/app/(pages)/Inventory/page.tsx
@@ -1,11 +1,31 @@
-
+import React from "react"
+import styles from './styles.module.css'
 
 const InventoryHomePage = () => {
   return (
-    <div className='bg-red-800 w-full h-full ml-[200px]'>
-      Inventory Home Page
-       /// your other components
-    </div>
+    // <div className='bg-red-800 w-full h-full ml-[200px]'>
+    //   Inventory Home Page
+    //    /// your other components
+    // </div>
+    <>
+      <h1 className={styles.h1}>InventoryHomePage</h1>
+
+      <div className={styles.dashboard}>
+        <div className={styles.dashboardContent}>
+          <p>Total Inventories: 10</p>
+          <p>Total Inventories: 10</p>
+          <p>Total Inventories: 10</p>
+      </div>
+        
+      </div>
+
+      <div className={styles.column}>
+        <button className={styles.button}> Inventory Management</button>
+        <button className={styles.button}> Report Generation</button>
+        <button className={styles.button}> Integrate with Sales Forecasting</button>
+      </div>
+    </>
+    
   )
 }
 
